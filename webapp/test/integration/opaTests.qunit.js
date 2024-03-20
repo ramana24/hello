6a2f4331bbcca01d897abb3afc3921fd544bd2c0
ramana24 <yellapuramana@gmail.com>
2024-03-20 07:56:42 +0530

    Hellow World version 1.0

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ux/hello/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ux/hello/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
